6a36d09c483756cdd73f08efaa97fa86e76541a0
Dilshani500 <dilshaniwee50@gmail.com>
2025-03-05 01:54:30 +0530

    Dashbord

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -15,14 +15,36 @@ const Dashboard = () => {
 
   return dashData && (
     <div className='m-5'>
-        <div>
-            <div>
-              <img src={assets.doctor_icon} alt=""/>
+        <div className='flex flex-wrap gap-3'>
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+              <img className='w-14' src={assets.doctor_icon} alt=""/>
               <div>
-                <p>{dashData.doctors}</p>
-                <p>Doctors</p>
+                <p className='text-xl font-semibold text-gray-600'>{dashData.doctors}</p>
+                <p className='text-gray-400'>Doctors</p>
               </div>
             </div>
+
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+              <img className='w-14' src={assets.appointments_icon} alt=""/>
+              <div>
+                <p  className='text-xl font-semibold text-gray-600'>{dashData.appointments}</p>
+                <p className='text-gray-400'>Appointments</p>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+              <img className='w-14' src={assets.patients_icon} alt=""/>
+              <div>
+                <p  className='text-xl font-semibold text-gray-600'>{dashData.patients}</p>
+                <p className='text-gray-400'>patients</p>
+              </div>
+            </div>
+        </div>
+        <div className='bg-white '>
+          <div className='flex items-center gap-2.5 px-4 py-4 mt-10 rounded'>
+            <img src={assets.list_icon} alt=""/>
+            <p>Latest Bookings</p>
+          </div>
         </div>
     </div>
   )
